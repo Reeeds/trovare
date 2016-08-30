f67fe8d5f59ab238104a9137e986d9f525b42137
Sandro Roth [LU14843] <sandro.roth@lukb.ch>
2016-08-30 14:04:11 +0200

    Implement sort functionality

--- src/app/issuelist/issuelist.component.js
+++ src/app/issuelist/issuelist.component.js
@@ -13,19 +13,26 @@
   function IssueListController(csv, filterColumns) {
     var vm = this;
 
-    vm.content = "";
-    vm.contentJson = "";
+    vm.content      = "";
+    vm.contentJson  = "";
     vm.sortType     = 'fish'; // set the default sort type
     vm.sortReverse  = false;  // set the default sort order
-    vm.searchFish   = '';     // set the default search/filter term
-    vm.fileLoaded = fileLoaded;
-    vm.filters = {};
+    vm.search       = '';     // set the default search/filter term
+    vm.filters      = {};
+    vm.headers      = [];
+
+    vm.fileLoaded   = fileLoaded;
+    vm.orderBy = orderBy;
 
     //////////
 
     function fileLoaded(fileContent) {
       vm.content = fileContent;
       vm.contentJson = new csv(fileContent, { header: true }).parse();
+
+      angular.forEach(vm.contentJson[0], function(val, key) {
+        vm.headers.push({ title: key });
+      });
 
       angular.forEach(filterColumns, function(filterColumn) {
         vm.filters[filterColumn] = { name: filterColumn, items: [], values: [] };
@@ -41,6 +48,10 @@
           }
         });
       });
+    }
+
+    function orderBy(val) {
+      return val[vm.sortType];
     }
   }
 })();
