d9dc4eb3d9c29d811c03c38955d45784bcaf8ac8
Sandro Roth [LU14843] <sandro.roth@lukb.ch>
2016-09-01 08:15:30 +0200

    Improve search and remove filters

--- src/app/issuelist/issuelist.component.js
+++ src/app/issuelist/issuelist.component.js
@@ -18,7 +18,6 @@
     vm.sortType     = 'fish'; // set the default sort type
     vm.sortReverse  = false;  // set the default sort order
     vm.search       = '';     // set the default search/filter term
-    vm.filters      = {};
     vm.headers      = [];
 
     vm.fileLoaded   = fileLoaded;
@@ -32,21 +31,6 @@
 
       angular.forEach(vm.contentJson[0], function(val, key) {
         vm.headers.push({ title: key });
-      });
-
-      angular.forEach(filterColumns, function(filterColumn) {
-        vm.filters[filterColumn] = { name: filterColumn, items: [], values: [] };
-      });
-
-      angular.forEach(vm.contentJson, function(row, index) {
-        angular.forEach(row, function(itemVal, itemName) {
-          if (filterColumns.indexOf(itemName) !== -1) {
-            if (vm.filters[itemName].values.indexOf(itemVal) === -1) {
-              vm.filters[itemName].items.push({ val: itemVal });
-              vm.filters[itemName].values.push(itemVal);
-            }
-          }
-        });
       });
     }
 
--- src/app/issuelist/multisearch.filter.js
+++ src/app/issuelist/multisearch.filter.js
@@ -10,28 +10,52 @@ angular
       if (searchText.trim() === "") return items;
 
       var filteredItems = new Array();
-      var searchItems = splitSearchText(searchText);
+      var searchItems = parseSearchText(searchText);
 
       angular.forEach(items, function(row) {
-        var foundAllSearchItems = true;
+        var isRowSearchResult = true;
 
-        angular.forEach(searchItems, function(searchItem) {
-          var searchItemFound = false;
+        // Check for key search
+        angular.forEach(row, function(val, key) {
+          var searchKey = key.toString().toLowerCase().replace(/[\s]*/g, "");
 
-          angular.forEach(row, function(val) {
-            if (val.toString().toLowerCase().indexOf(searchItem.toString().toLowerCase()) !== -1) {
-              searchItemFound = true;
-              return false;
+          angular.forEach(searchItems["keySearch"], function(keySearch) {
+            if (keySearch.first.toString().toLowerCase().replace(/[\s]*/g, "") === searchKey) {
+              if (val.toString().toLowerCase().indexOf(keySearch.second.toString().toLowerCase()) === -1) {
+                isRowSearchResult = false;
+                return false;
+              }
             }
           });
 
-          if (searchItemFound !== true) {
-            foundAllSearchItems = false;
+          if (!isRowSearchResult) return false;
+        });
+
+        // Check for excluded Words
+        angular.forEach(searchItems["exclude"], function(excludeItem) {
+          if (containsRowString(row, excludeItem)) {
+            isRowSearchResult = false;
             return false;
           }
         });
 
-        if (foundAllSearchItems) {
+        // Check for matches
+        angular.forEach(searchItems["match"], function(matchItem) {
+          if (!containsRowString(row, matchItem)) {
+            isRowSearchResult = false;
+            return false;
+          }
+        });
+
+        // Check for or-matches
+        angular.forEach(searchItems["matchOr"], function(orMatch) {
+          if (!containsRowString(row, orMatch.first) && !containsRowString(row, orMatch.second)) {
+            isRowSearchResult = false;
+            return false;
+          }
+        });
+
+        if (isRowSearchResult) {
           filteredItems.push(row);
         }
       });
@@ -40,39 +64,83 @@ angular
     }
   }
 
-  function splitSearchText(searchText) {
-    var items = new Array();
-    var isContiguousString = false;
-    var currentItem = "";
+  function containsRowString(row, string) {
+    var isStringInRow = false;
 
-    for(var idx = 0; idx < searchText.length; idx++) {
-      var char = searchText.substr(idx, 1);
-
-      switch(char) {
-        case '"':
-          isContiguousString = !isContiguousString;
-          break;
-
-        case ' ':
-          if (!isContiguousString && currentItem !== "") {
-            items.push(currentItem);
-            currentItem = "";
-          } else {
-            currentItem += char;
-          }
-          break;
-
-        default:
-          currentItem += char;
-          break;
+    angular.forEach(row, function(val) {
+      if (val.toString().toLowerCase().indexOf(string.toString().toLowerCase()) !== -1) {
+        isStringInRow = true;
       }
-    }
+    });
 
-    if (currentItem !== "") {
-      items.push(currentItem);
-    }
+    return isStringInRow;
+  }
 
-    return items;
+
+  function parseSearchText(searchText) {
+    var matches = {};
+    var output = {};
+
+    // --"EXCLUDE WORDS"
+    output = checkPattern(/(?:\-\-)(?:(?:")([^\"]*)(?:\"))/g, searchText, 1);
+    matches["exclude"] = output.matches;
+    searchText = output.text;
+
+    // "MATCH THIS"|"OR THIS"
+    output = checkPattern(/\"([^\|\"]+)\"\|\"([^\"]+)\"/g, searchText, 1, 2);
+    matches["matchOr"] = output.matches;
+    searchText = output.text;
+
+    // COLUMNKEY:"VALUE WITH SPACE"
+    output = checkPattern(/\b([^\s\"]+):\"([^\"]+)\"/g, searchText, 1, 2);
+    matches["keySearch"] = output.matches;
+    searchText = output.text;
+
+    // "MATCH WORDS"
+    output = checkPattern(/(?:(?:")([^\"]*)(?:\"))/g, searchText, 1);
+    matches["match"] = output.matches;
+    searchText = output.text;
+
+    // --EXCLUDETHIS
+    output = checkPattern(/(?:\-\-)([^\s]*)/g, searchText, 1);
+    matches["exclude"] = matches["exclude"].concat(output.matches);
+    searchText = output.text;
+
+    // MATCHTHIS|ORTHIS
+    output = checkPattern(/\b([^\|][\S]+)\|([\S]+)\b/g, searchText, 1, 2);
+    matches["matchOr"] = matches["matchOr"].concat(output.matches);
+    searchText = output.text;
+
+    // COLUMNKEY:VALUE
+    output = checkPattern(/\b([^\s]+):([^\s]+)\b/g, searchText, 1, 2);
+    matches["keySearch"] = matches["keySearch"].concat(output.matches);
+    searchText = output.text;
+
+    // MATCHTHIS
+    output = checkPattern(/\b[\S]+\b/g, searchText, 0);
+    matches["match"] = matches["match"].concat(output.matches);
+    searchText = output.text;
+
+    return matches;
+  }
+
+  function checkPattern(pattern, text, index, index2) {
+      var result = { matches: [], text: "" }
+      var match;
+
+      do {
+          match = pattern.exec(text);
+          if (match) {
+          		if (typeof(index2) !== 'undefined') {
+              	result.matches.push({ "first": match[index], "second": match[index2] });
+              } else {
+              	result.matches.push(match[index]);
+              }
+          }
+      } while (match);
+
+      result.text = text.replace(pattern, "");
+      return result;
   }
 
 })();
